85fa5de612f2d025abbaac9549b97d752cd200c6
Paul Klemstine <paulklemstine@gmail.com>
2022-08-27 12:36:59 -0500

    trying to access classpath

--- cheerpj/run.sh
+++ cheerpj/run.sh
@@ -1,1 +1,1 @@
-C:/Users/Paul/Documents/Dominion/cheerpj/cheerpjfy.py C:/Users/Paul/Documents/Dominion/build/libs/Dominion-1.0-SNAPSHOT-all.jar && echo "ready..."&& python3 -m http.server 8081
+C:/Users/Paul/Documents/Dominion/cheerpj/cheerpjfy.py -j 6 C:/Users/Paul/Documents/Dominion/build/libs/Dominion-1.0-SNAPSHOT-all.jar && echo "ready..."&& python3 -m http.server 8081
