f576ddb83fdc72e99781cd9cc97aad3a11583ff7
Paul Klemstine <paulklemstine@gmail.com>
2022-09-01 14:50:48 -0500

    wip

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-./gradlew fatJar
-cd ./build/libs && C:/Users/Paul/Documents/Dominion/cheerpj/cheerpjfy.py --deps ../../cheerpj/cheerpj-dom.jar C:/Users/Paul/Documents/Dominion/build/libs/Dominion-1.0-SNAPSHOT-all.jar && echo "ready..."&& python3 -m http.server 8081
+./gradlew fatJar && cd ./build/libs && C:/Users/Paul/Documents/Dominion/cheerpj/cheerpjfy.py --deps ../../cheerpj/cheerpj-dom.jar C:/Users/Paul/Documents/Dominion/build/libs/Dominion-1.0-SNAPSHOT-all.jar && echo "ready..."&& python3 -m http.server 8081
